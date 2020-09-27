b07b724eeb1e1360bd4ef3457ac09b215b42d2f4
Stepan Sukach <stepansukac@gmail.com>
2020-09-27 22:00:30 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function reverse (n) {
-  
+   let array = n.toString().split('');
+
+   if (array[0] === '-') {
+      array.splice(0, 1)
+   };
+
+   if (array[array.length - 1] === '0') {
+      array.splice(-1, 1)
+   };
+   
+   let result = array.reverse().join('');
+   return +result;
 }
+
+
+/* function reverse (n) {
+   let array = n.toString().split('');
+
+   if (array[0] === '-') {
+      array.splice(0, 1)
+   };
+
+   if (array[array.length - 1] === '0') {
+      array.splice(-1, 1)
+   };
+   
+   let result = array.reverse().join('');
+   return +result;
+}
+
+console.log(reverse(162)) */
