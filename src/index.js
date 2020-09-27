15aaab8c28613216988814b3c3b9ba596c1ebdd9
Stepan Sukach <stepansukac@gmail.com>
2020-09-27 22:03:47 +0300

    fix: correct code

--- src/index.js
+++ src/index.js
@@ -13,20 +13,3 @@ module.exports = function reverse (n) {
    return +result;
 }
 
-
-/* function reverse (n) {
-   let array = n.toString().split('');
-
-   if (array[0] === '-') {
-      array.splice(0, 1)
-   };
-
-   if (array[array.length - 1] === '0') {
-      array.splice(-1, 1)
-   };
-   
-   let result = array.reverse().join('');
-   return +result;
-}
-
-console.log(reverse(162)) */
